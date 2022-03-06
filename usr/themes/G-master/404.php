46cac07e88ba0bde8bb1a4b1ab2e0c9dd40e3477
泠夜Soul <2562384310@qq.com>
2022-03-06 09:55:38 +0800

    123
    123

---
+++ usr/themes/G-master/404.php
@@ -0,0 +1,23 @@
+<?php $this->need('header.php'); ?>
+
+<div id="zp">
+    <div id="zp-content">
+      <?php $this->content();?>
+      <body>
+    <p>
+      
+      <iframe src=https://niconiconi.cc/404.html id="tag" name="tag" allowTransparency="true" width=100% height=100% scrolling="no" frameborder="0">
+        </iframe>
+  
+  </p>
+</body>
+    </div>
+  </div>
+
+
+
+
+
+
+
+	<?php $this->need('footer.php'); ?>
